6b1035c9be517ea70ce910dc3ccd077b807d6c94
mohamedeld <mohamed.azoz20010@gmail.com>
2023-11-15 14:02:22 +0200

    add an awesome leaflet library to show map in our app

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,15 +1,41 @@
+import { useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { useNavigate, useSearchParams } from "react-router-dom";
+import { useCities } from "../contexts/CitiesContext";
 import styles from "./Map.module.css";
 export default function Map() {
+  const { cities } = useCities();
   const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
+  const [mapPosition, setMapPosition] = useState([40, 0]);
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
-  console.log(lat);
-  console.log(lng);
+
   return (
     <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      Map
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => {
+          return (
+            <Marker
+              position={[city.position.lat, city.position.lng]}
+              key={city.id}
+            >
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          );
+        })}
+      </MapContainer>
     </div>
   );
 }
